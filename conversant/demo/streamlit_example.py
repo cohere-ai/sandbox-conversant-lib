dec710f83996dc8a2f14dfb4b22b7c01791e33f7
mads <madelinejenkins@cohere.com>
2022-12-16 17:11:20 +0000

    bug: Streamlit demo with custom personas now works properly, closes CNV-92 (#79)
    Specify custom_persona_dir in call to PromtChatbot.from_persona
    Only pass CUSTOM_PERSONA_DIRECTORY as an argument if it is 
    not None, otherwise use PERSONA_MODEL_DIR.
    
    Co-authored-by: yichern <32623504+yichern@users.noreply.github.com>

--- conversant/demo/streamlit_example.py
+++ conversant/demo/streamlit_example.py
@@ -17,7 +17,7 @@ import emoji
 import streamlit as st
 
 from conversant.demo import ui, utils
-from conversant.prompt_chatbot import PromptChatbot
+from conversant.prompt_chatbot import PERSONA_MODEL_DIRECTORY, PromptChatbot
 from conversant.utils import demo_utils
 
 # Set a custom persona directory by changing the following line
@@ -48,6 +48,9 @@ def initialize_chatbot() -> None:
         st.session_state.bot = PromptChatbot.from_persona(
             emoji.replace_emoji(st.session_state.persona, "").strip(),
             client=cohere.Client(os.environ.get("COHERE_API_KEY")),
+            persona_dir=CUSTOM_PERSONA_DIRECTORY
+            if CUSTOM_PERSONA_DIRECTORY
+            else PERSONA_MODEL_DIRECTORY,
         )
     if "bot" in st.session_state and st.session_state.bot:
         update_session_with_prompt()
